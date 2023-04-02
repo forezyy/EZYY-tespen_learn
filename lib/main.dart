5fd0e1b00aa35f243eec1eaa3daa4d3e072ea0aa
Arif Iskandar <arif.iskandar.ext@salt.co.id>
2023-04-02 13:48:04 +0700

    Remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const HomeScreen());
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
